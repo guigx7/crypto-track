2e65d00dae083db99d779a30c8debc3c807f81bb
Guilherme Abdelnor <guigo211103@gmail.com>
2025-11-17 02:44:44 -0300

    feat: configure React Query provider and API client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./styles/global.css";
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './core/state/store.ts'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </Provider>
 )
